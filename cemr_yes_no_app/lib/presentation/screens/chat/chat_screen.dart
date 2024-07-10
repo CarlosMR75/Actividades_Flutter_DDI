4f8e7c7efe7b6868aafd672240e1ed6971b2441e
CarlosMR75 <carlosemr75@gmail.com>
2024-07-10 12:33:45 -0600

    First commit

---
+++ cemr_yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://th.bing.com/th/id/OIG2.bkY9jan0qM0SIssaGOsl?pid=ImgGn'),
+          ),
+        ),
+        title: const Text('Usuario'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    controller: chatProvider.chatScrollController,
+                    itemCount: chatProvider.messages.length,
+                    itemBuilder: (context, index) {
+                      final message = chatProvider.messages[index];
+
+                      return message.fromWho == FromWho.hers
+                          ? HerMessageBubble(message: message)
+                          : MyMessageBubble(message: message);
+                    })),
+            MessageFieldBox(
+              // onValue: (value) => chatProvider.sendMessage(value),
+              onValue: chatProvider.sendMessage,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ hello_world_carlos_mata/android/app/src/main/kotlin/com/example/hello_world_carlos_mata/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hello_world_carlos_mata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
